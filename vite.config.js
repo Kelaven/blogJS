6ae1253998d451594c15683ac9347f30d738e8e6
Kelaven <kevin.lavenant@live.fr>
2024-07-25 11:03:49 +0200

    feat(base): multipage launched

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from "path"; // ! pour build le multipages
 
 export default defineConfig({
     root: "src",
     build: {
         outDir: '../dist',
+        rollupOptions: { // ! pour build le multipages
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                form: resolve(__dirname, 'src/form/form.html'),
+            }
+        }
     },
 });
