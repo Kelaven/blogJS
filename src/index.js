a0e47afaa605cb300641339d5f6de849cccf259c
Kelaven <kevin.lavenant@live.fr>
2024-09-05 17:12:58 +0200

    feat(homepage): articles displayed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./index.scss";
 const articleContainerElement = document.querySelector(".articles-container");
 
 const createArticles = (articles) => {
-    const articlesDOM = articles.map(article => {
+    const articlesDOM = articles.map((article) => {
         const articleDOM = document.createElement('div');
         articleDOM.classList.add("article");
         articleDOM.innerHTML = `
@@ -15,21 +15,43 @@ const createArticles = (articles) => {
 <p class="article-author">${article.author} - ${article.category}</p>
 <p class="article-content">${article.content}</p>
 <div class="article-actions">
-  <button class="btn btn-danger" data-id=${article.id}>Supprimer</button>
+  <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
 </div>`
-        return articleDOM
+        return articleDOM;
     });
     articleContainerElement.innerHTML = '';
     articleContainerElement.append(...articlesDOM);
+    const deleteButtons = articleContainerElement.querySelectorAll('.btn-danger');
+    deleteButtons.forEach(button => {
+        button.addEventListener("click", async event => {
+            try {
+                const target = event.target;
+                const articleID = target.dataset.id;
+                const response = await fetch(`https://restapi.fr/api/articles/${articleID}`, {
+                    method: "DELETE"
+                });
+                const body = await response.json();
+                // quand on supprime un article, on relance l'affichage de la liste des articles mis à jour :
+                fetchArticles();
+            } catch (error) {
+                console.log('e : ', error);
+            }
+
+        })
+    })
+
 };
 
 // Récupérer la liste de nos articles : 
 const fetchArticles = async () => {
     try {
         const response = await fetch("https://restapi.fr/api/articles");
-        const articles = await response.json();
+        let articles = await response.json();
         console.log(articles);
-
+        // Restapi retourne un objet s'il n'y a qu'un seul article, nous devons donc le transformer en tableau :
+        if (!Array.isArray(articles)) {
+            articles = [articles];
+        }
         // transformer le json en élément du DOM :
         createArticles(articles);
     } catch (e) {
